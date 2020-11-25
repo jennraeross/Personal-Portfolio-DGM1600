d5ce63a2c6cf9699d835cd54fde1c793f00d3e84
Jennifer Roylance <jenn.rae.ross@gmail.com>
2020-11-24 17:47:05 -0700

    changes json to array of objects

--- js/tarot.js
+++ js/tarot.js
@@ -1,19 +1,19 @@
+// Selects the div where cards will be shown
 const cards = document.querySelector("#cards");
-function Card(entry) {
-    this.name = entry.name,
-    this.summary = entry.summary,
-    this.full_meaning = entry.full_meaning,
-    this.upright = entry.upright,
-    this.reversed = entry.reversed,
-    this.image = entry.image
+
+// Array that will be filled with card objects.
+let deck = [];
+
+// Clears cards div
+function clearCards() {
+    cards.innerHTML = "";
 }
-fetch('https://tarot.howlcode.com/api/v1/cards')
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        data.forEach(info => {
-            cards.innerHTML +=`
+
+// Takes an array of card objects and displays them.
+function displayCards(cardsArray, type) {
+    clearCards();
+    cardsArray.forEach(info => {
+        cards.innerHTML +=`
             <div class="card">
                 <div class="content">
                     <div class="front"><img src="${info.image}" /></div>
@@ -21,8 +21,33 @@ fetch('https://tarot.howlcode.com/api/v1/cards')
                 </div>
             </div>
             `
-        });
-    })
-    .catch((err) => {
-        alert("Could not retrieve tarot cards");
-    })
+    });
+}
+
+//* Retrieves card data from API
+async function fetchCards() {
+    const response = await fetch('https://tarot.howlcode.com/api/v1/cards');
+    // watches for errors
+    if (!response.ok) {
+        const message = `An error has occured: '${response.status}'. Please try again later.`;
+        throw new Error(message);
+    }
+    //creates new object when response is recieved, and returns it.
+    const cardsJSON = await response.json();
+    return cardsJSON;
+}
+
+// Alerts the user if there is an error.
+fetchCards().catch(error => {
+    alert(error.message);
+});
+
+// Uses fetchCards() to get card information, 
+// converts the response to an array of objects,
+// and inserts said information into the page.
+fetchCards().then(cardsJSON => {
+    // Converts to array of objects
+    deck = Object.values(cardsJSON);
+    // Inserts cards into page.
+    displayCards(deck);
+});
